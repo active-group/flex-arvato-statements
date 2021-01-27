32891ebe0ed6bc149203bf24259fa5bd57c7f91b
Mike Sperber <sperber@deinprogramm.de>
2021-01-27 18:11:34 +0100

    Connect other nodes.

--- src/erlbank_monolithic_app.erl
+++ src/erlbank_monolithic_app.erl
@@ -21,11 +21,26 @@ start_cowboy() ->
                                  [{port, 8000}],
                                  #{env => #{dispatch => Dispatch}}).
 
+keep_pinging(Node) ->
+    case net_adm:ping(Node) of
+        pong -> lager:info("successfully connected with node ~p", [Node]),
+                ok;
+        _ -> lager:info("re-pinging node ~p", [Node]),
+             timer:sleep(1000),
+             keep_pinging(Node)
+    end.
 
 start(_StartType, _StartArgs) ->
     database:init_database(),
     lager:start(),
     start_cowboy(),
+
+    AccountNode = list_to_atom(os:getenv("ACCOUNT_NODE")),
+    TransactionNode = list_to_atom(os:getenv("TRANSACTION_NODE")),
+    keep_pinging(AccountNode),
+    keep_pinging(TransactionNode),
+
+    lager:info("starting statements processes"),
     account_poller:poll_process(),
     statements_server:start(ok),
     erlbank_monolithic_sup:start_link().
